2311efbbee04dad9a58a79923ea69eb543cddcdc
Martin Perez Bazan <martin.perezbazan@gmail.com>
2022-11-01 01:08:33 -0300

    Update to ES6 and modules

---
+++ modules/Awesome.js
@@ -0,0 +1,6 @@
+export default class Awesome {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
---
+++ modules/selectors.js
@@ -0,0 +1,27 @@
+const booksContainer = document.querySelector('.books-container');
+const message = document.querySelector('.message');
+const addBtn = document.querySelector('#addButton');
+const listLink = document.querySelector('#list');
+const addNewLink = document.querySelector('#addNew');
+const contactLink = document.querySelector('#contact');
+const seeList = document.querySelector('#seeList');
+const sectionOne = document.querySelector('#sectionone');
+const sectionTwo = document.querySelector('#sectiontwo');
+const sectionThree = document.querySelector('#sectionthree');
+const close = document.querySelector('#close');
+const time = document.querySelector('#timeText');
+
+export {
+  booksContainer,
+  message,
+  addBtn,
+  listLink,
+  addNewLink,
+  contactLink,
+  seeList,
+  sectionOne,
+  sectionTwo,
+  sectionThree,
+  close,
+  time,
+};
